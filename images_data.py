b55ef1ceb55124360a05fe065a4d1d0d92c1dee9
Taktik05 <146109939+Taktik05@users.noreply.github.com>
2026-05-05 15:10:03 +0300

    Create images_data.py

---
+++ images_data.py
@@ -0,0 +1,8 @@
+TEXTBOOK_IMAGES = {
+    "hsk_standard": "hsk_standard.png",
+    "developing_chinese": "developing_chinese.png",
+    "boya_chinese": "boya_chinese.png",
+    "easy_steps": "easy_steps.png",
+    "integrated_chinese": "integrated_chinese.png",
+    "all_collection": "all_collection.png",
+}
