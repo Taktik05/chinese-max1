92030794535cf82981c3b1ec61020c9a45aa4291
Taktik05 <146109939+Taktik05@users.noreply.github.com>
2026-05-24 10:21:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from aiogram import Bot, Dispatcher
 from commands import router as commands_router
 from main_menu import router as main_menu_router
 from textbooks import router as textbooks_router
+from help_search import router as help_search_router
 
 logging.basicConfig(level=logging.INFO)
 
@@ -19,6 +20,7 @@ async def main():
     dp.include_router(commands_router)
     dp.include_router(main_menu_router)
     dp.include_router(textbooks_router)
+    dp.include_router(help_search_router)  # ДОБАВИТЬ
     
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
